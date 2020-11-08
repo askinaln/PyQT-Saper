ab807df6d7ab33df7d5c1ac3fc3056fd74fe6216
askinaln <arzamaskina.ja@gmail.com>
2020-11-08 23:08:08 +0300

    Таймер дописан.
    Исправлен мелкие недочеты(кнопку можно нажмать только один раз, ошибки в названиях и тп)
    
    Функция конца игры.
    
    Осталось написать: функция "Раскрытие всех полей после окончания игры", БД с количеством побед\поражений

--- Сапер.py
+++ Сапер.py
@@ -10,7 +10,7 @@ LEVELS = [(8, 10), (16, 40), (20, 70)]  # новичок, любитель, пр
 class Timer(QMainWindow):
     timeout = QtCore.pyqtSignal()
 
-    def __init__(self, lcd=None, sit=False):
+    def __init__(self, lcd=None):
         super(Timer, self).__init__()
         self.timeViewer = lcd
 
@@ -20,10 +20,6 @@ class Timer(QMainWindow):
         self.timerUp = QtCore.QTimer()
         self.timerUp.setInterval(self.timeInterval)
         self.timerUp.timeout.connect(self.updateUptime)
-
-        self.timerUp.start()
-
-        # self.pushButton_5.clicked.connect(self.timerUp.stop)  # НОВАЯ ИГРА ИЛИ ПРОИГРЫШ
 
     def setUnit(self):
         self.timeInterval = 1000
@@ -50,6 +46,8 @@ class Saper(Timer, QMainWindow):
 
         self.buttons = []  # список всех кнопок на игровом поле
 
+        self.timer = Timer(self.timeViewer)  # Таймер для игры
+
         self.reset_pole.clicked.connect(self.new_game)
         self.level_1.clicked.connect(self.game_size)
         self.level_2.clicked.connect(self.game_size)
@@ -62,10 +60,12 @@ class Saper(Timer, QMainWindow):
             self.len_pole, self.kolvo_mines = LEVELS[1]
         elif self.sender().text() == 'Профессионал':
             self.len_pole, self.kolvo_mines = LEVELS[2]
+        self.kolvo_bad = self.kolvo_mines  # количество кнопок на которые уже нельзя нажимать
         self.new_game()
 
     def new_game(self):  # сброс всех полей, новая игра
         # self.gridLayout.
+        self.label.setText(' ' * 30)
         for row in self.buttons:
             for elem in row:
                 self.gridLayout.removeWidget(elem)
@@ -80,10 +80,14 @@ class Saper(Timer, QMainWindow):
                 self.gridLayout.addWidget(btn_pole, x, y)
         self.lcdNumber.display(self.kolvo_mines)  # выводим количество мин на поле
         self.mines_spread()  # разбрасываем мины на поле
-        self.timer_()  # начинаем отсчет времени игры
+        self.timer_(True)  # начинаем отсчет времени игры
 
-    def timer_(self):  # Начало отсчета время игры
-        timer = Timer(self.timeViewer, True)
+    def timer_(self, sit):  # Начало\конец отсчета время игры
+        if sit:
+            self.timer.time = 0
+            self.timer.timerUp.start()
+        else:
+            self.timer.timerUp.stop()
 
     def mines_spread(self):  # разбрасываем мины на поле
         self.cells = [['0' for j in range(self.len_pole)] for i in range(self.len_pole)]  # создаем список всех ячеек,
@@ -95,11 +99,10 @@ class Saper(Timer, QMainWindow):
             self.cells[row][col] = '1'
 
     def mine_or_no(self):  # проверка мина или нет
-        for row_ in range(len(self.buttons)):
-            for i in range(len(self.buttons)):
-                if self.buttons[row_][i] == self.sender():
-                    row, col = row_, i
-                    break
+        for row_ in range(self.len_pole):
+            if self.sender() in self.buttons[row_]:
+                row, col = row_, self.buttons[row_].index(self.sender())
+                break
         if self.cells[row][col] == '1':
             self.the_end_game(row, col)
         else:
@@ -108,22 +111,22 @@ class Saper(Timer, QMainWindow):
     def count_cells(self, r, c):  # Клетки, окружающие нажатую клетку
         if r == 0 == c:  # Левая верхняя клетка
             data = [self.cells[0][1], self.cells[1][2], self.cells[1][0]]
-        elif r == self.len_pole and c == 0:  # Левая нижняя
+        elif r == self.len_pole - 1 and c == 0:  # Левая нижняя
             data = [self.cells[r - 1][0], self.cells[r - 1][1], self.cells[r][1]]
-        elif r == self.len_pole and c == self.len_pole:  # Правая нижняя
+        elif r == self.len_pole - 1 and c == self.len_pole - 1:  # Правая нижняя
             data = [self.cells[r][c - 1], self.cells[r - 1][c - 1], self.cells[r][c]]
-        elif r == 0 and c == self.len_pole:  # Верхняя правая
+        elif r == 0 and c == self.len_pole - 1:  # Верхняя правая
             data = [self.cells[0][c - 1], self.cells[1][c - 1], self.cells[1][c]]
-        elif (0 < r < self.len_pole) and c == 0:  # Клетка в левом ряду
+        elif (0 < r < self.len_pole - 1) and c == 0:  # Клетка в левом ряду
             data = [self.cells[r + 1][0], self.cells[r - 1][0], self.cells[r + 1][1],
                     self.cells[r - 1][1], self.cells[r][1]]
-        elif (0 < r < self.len_pole) and c == self.len_pole:  # Клетка в правом ряду
+        elif (0 < r < self.len_pole - 1) and c == self.len_pole - 1:  # Клетка в правом ряду
             data = [self.cells[r + 1][c], self.cells[r - 1][c], self.cells[r + 1][c - 1],
                     self.cells[r - 1][c - 1], self.cells[r][c - 1]]
-        elif r == self.len_pole and (0 < c < self.len_pole):  # Клетка в нижнем ряду
+        elif r == self.len_pole - 1 and (0 < c < self.len_pole - 1):  # Клетка в нижнем ряду
             data = [self.cells[r][c - 1], self.cells[r][c + 1], self.cells[r - 1][c - 1],
                     self.cells[r - 1][c], self.cells[r - 1][c + 1]]
-        elif r == 0 and (0 < c < self.len_pole):  # Клетка в верхнем ряду
+        elif r == 0 and (0 < c < self.len_pole - 1):  # Клетка в верхнем ряду
             data = [self.cells[r][c - 1], self.cells[r][c + 1], self.cells[r + 1][c - 1],
                     self.cells[r + 1][c], self.cells[r + 1][c + 1]]
         else:  # Любая другая клетка
@@ -139,10 +142,28 @@ class Saper(Timer, QMainWindow):
                 kolvo_mine += 1
         self.cell_print(r, c, kolvo_mine)
 
-    def cell_print(self, r, c, kolvo):
+    def cell_print(self, r, c, kolvo):  # Вывод информации на клетку
         self.buttons[r][c].setText(str(kolvo))
+        self.buttons[r][c].setEnabled(False)
+        self.kolvo_bad += 1
+        self.kolvo_operating_buttons()
 
-    def the_end_game(self, r, c): # Проигрыш
+    def kolvo_operating_buttons(self):  # Подсчет - выиграл ли игрок
+        if self.kolvo_bad == self.len_pole * self.len_pole:
+            self.the_end_game()
+
+    def the_end_game(self, r=None, c=None):  # Конец игры
+        for row in self.buttons:
+            for btn in row:
+                btn.setEnabled(False)
+        self.opening()
+        if r is None:
+            self.label.setText('Поздравляю! Ты выиграл!')
+        else:
+            self.label.setText('Ты проиграл!')
+        self.timer_(False)
+
+    def opening(self):  # Вскрытие всех клеток, в случае проигрыша\выигрыша
         pass
 
 
